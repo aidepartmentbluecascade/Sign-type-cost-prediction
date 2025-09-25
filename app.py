73b050d4e25201cf310d67c31948540dfa4864c1
aidepartmentbluecascade <your-email@example.com>
2025-09-25 11:38:23 +0500

    UI changes

--- app.py
+++ app.py
@@ -85,5 +85,8 @@ def index():
                            models=list(model_results.keys()))
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default 5000 for local
+    app.run(host="0.0.0.0", port=port, debug=True)
